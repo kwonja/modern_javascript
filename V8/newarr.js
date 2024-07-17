3ac09f281d2d5e6e449fe84a7659c628c26497ef
kwonja <kownja87@gmail.com>
2024-07-17 18:00:00 +0900

    파일이동

--- V8/newarr.js
+++
@@ -1,7 +0,0 @@
-console.time("arr")
-let arr = new Array(10000000);
-for(let i=0;i<10000000;i++)
-{
-    arr[i]=0;
-}
-console.timeEnd("arr")
--- V8/obj.js
+++
@@ -1,8 +0,0 @@
-arr={};
-
-console.time("obj")
-for(let i=0;i<100000000;i++)
-{
-    arr[i]=0;
-}
-console.timeEnd("obj") //obj: 2.465s
